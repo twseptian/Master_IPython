757b5d710444162638bc8d960270256009078b21
Maddosaurus <me@sigpwr.de>
2018-07-19 16:03:14 +0200

    fix sdn-dnn to draw the correct models

--- implementations/sdn-dnn.py
+++ implementations/sdn-dnn.py
@@ -26,16 +26,17 @@ plot_model(model, to_file='model-sdn-dnn.png', show_layer_names=True, show_shape
 #model.fit(data, labels, epochs=100, batch_size=10)
 
 
+
 # Not sure if this is the right way. It could be that we actually only have 4 layers?
-# -> If 4: 
+# -> If 4, would that be multiclass with only two classes?!
 alt_model = Sequential()
 alt_model.add(Dense(12, activation='relu', input_dim=6))
 alt_model.add(Dense(6, activation='relu'))
 alt_model.add(Dense(3, activation='relu'))
 alt_model.add(Dense(2, activation='softmax'))
-alt_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+alt_model.compile(RMSprop(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 alt_model.summary()
-plot_model(model, to_file='model-sdn-dnn-alt.png', show_layer_names=True, show_shapes=True)
+plot_model(alt_model, to_file='model-sdn-dnn-alt.png', show_layer_names=True, show_shapes=True)
 
 #alt_model.fit(data, labels, epochs=100, batch_size=10)
